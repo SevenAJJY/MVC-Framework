af2d2b60c59cfb39d550795cf2d2e339291819d4
SevenAJJY <yassine.elhajjy@gmail.com>
2023-11-17 13:39:03 +0100

    Added not found action

--- app/controllers/notfoundcontroller.php
+++ app/controllers/notfoundcontroller.php
@@ -6,7 +6,8 @@ namespace SEVENAJJY\Controllers;
 class NotFoundController extends AbstractController
 {
     public function notFoundAction()
-    {
-        $this->_renderView();
-    }
+        {
+            $this->_language->load('template.common');
+            $this->_renderView();
+        }
 }
