01041c9860e627955c86afc679f76c99517ab317
SevenAJJY <yassine.elhajjy@gmail.com>
2023-11-17 13:30:02 +0100

    Change database name

--- app/config/config.php
+++ app/config/config.php
@@ -14,7 +14,7 @@
     defined('DATABASE_HOST_NAME')       ? null : define ('DATABASE_HOST_NAME', 'localhost');
     defined('DATABASE_USERNAME')        ? null : define ('DATABASE_USERNAME', 'root');
     defined('DATABASE_PASSWORD')        ? null : define ('DATABASE_PASSWORD', '');
-    defined('DATABASE_DB_NAME')         ? null : define ('DATABASE_DB_NAME', 'mvc_db');
+    defined('DATABASE_DB_NAME')         ? null : define ('DATABASE_DB_NAME', 'store_db');
     defined('DATABASE_PORT_NUMBER')     ? null : define ('DATABASE_PORT_NUMBER', 3306);
     defined('DATABASE_CONN_DRIVER')     ? null : define ('DATABASE_CONN_DRIVER', 1);
     
