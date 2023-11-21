51eeb0b30035380c36b4c708276b2bef3238cd37
SevenAJJY <yassine.elhajjy@gmail.com>
2023-11-21 10:20:14 +0100

    added some styles to the not found page

--- app/views/notfound/notfound.view.php
+++ app/views/notfound/notfound.view.php
@@ -1,1 +1,6 @@
-<h1><?= $text_not_found ?></h1>
+<div class="not-found text-center">
+    <p>
+        <img src="/img/not_found_page.gif" alt="not_found">
+    </p>
+    <span><?= $text_not_found ;?> </span>
+</div>
