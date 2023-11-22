31cf9c44bf3f0d6af3c1876e4f4ef6ba742e6d2d
SevenAJJY <yassine.elhajjy@gmail.com>
2023-11-22 21:09:55 +0100

    Create a new instance of the registry and pass it through frontController

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ use SEVENAJJY\Library\SessionManager;
 use SEVENAJJY\Library\Language;
 use SEVENAJJY\LIBRARY\Template\Template;
 use SEVENAJJY\LIBRARY\FrontController;
+use SEVENAJJY\Library\Registry;
 
 defined('DS') or define('DS' , DIRECTORY_SEPARATOR);
 
@@ -19,15 +20,20 @@ $template_parts = require_once '..' . DS . 'app' . DS . 'config' . DS . 'templat
 $session = new SessionManager();
 $session->start();
 
-if (!isset($_SESSION['lang'])) {
-    $_SESSION['lang'] = APP_DEFAULT_LANGUAGE;
+if (!isset($session->lang)) {
+    $session->lang = APP_DEFAULT_LANGUAGE;
 }
 
 $template = new Template($template_parts);
 
 $language = new Language();
 
-$frontController = new FrontController($template, $language);
+$registry = Registry::getInstance();
+$registry->session = $session;
+$registry->language = $language;
+
+
+$frontController = new FrontController($template, $registry);
 
 $frontController->dispatch();
 ?>
