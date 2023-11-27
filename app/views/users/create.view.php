a4ab4c9dcdbf8fa062ab2e097725dea619b7c65e
SevenAJJY <yassine.elhajjy@gmail.com>
2023-11-27 17:37:48 +0100

    Added view files of the users

--- app/views/users/create.view.php
+++ app/views/users/create.view.php
@@ -1,56 +1,114 @@
 <div class="home-content">
     <div class="row">
-        <h1 class=" main-head"><?= $text_header ;?></h1>
+        <h1 class="main-head"><?php echo $text_header ;?></h1>
+
         <div class="col-md-12 d-flex align-items-center justify-content-center">
             <div class="my-container fuser">
-                <h4>New User</h4>
-                <form action="" class="appform row" method="POST" enctype="multipart/form-data"
+                <h4><?php echo $text_legend ?></h4>
+                <form action="" class="appform row" method="post" autocomplete="off" enctype="multipart/form-data"
                     enctype="application/x-www-form-urlencoded">
                     <div class="col-md-6 col-lg-6 col-sm-12">
                         <div class="input-box">
-                            <input type="text" spellcheck="false" class="FirstName" name="name" id="FirstName"
+                            <input type="text" spellcheck="false" class="FirstName" name="FirstName" id="FirstName"
                                 maxlength="10" required>
-                            <label for="name">name</label>
+                            <label for="FirstName"><?php echo $text_label_FirstName ?></label>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-6 col-sm-12">
                         <div class="input-box">
-                            <input type="text" spellcheck="false" name="address" id="LastName" maxlength="10" required>
-                            <label for="address">address</label>
+                            <input type="text" spellcheck="false" name="LastName" id="LastName" maxlength="10" required>
+                            <label for="LastName"><?php echo $text_label_LastName ?></label>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-6 col-sm-12">
                         <div class="input-box">
-                            <input type="number" spellcheck="false" class="Username" name="age" id="Username"
+                            <input type="text" spellcheck="false" class="Username" name="Username" id="Username"
                                 maxlength="30" required>
-                            <label for="age" class="labelerror">age</label>
+                            <label for="username" class="labelerror"><?php echo $text_label_Username ?></label>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-6 col-sm-12">
                         <div class="input-box">
-                            <input type="number" spellcheck="false" step="0.01" id="Number" name="salary" required>
-                            <label for="Number">Salary</label>
+                            <input type="text" spellcheck="false" id="Number" name="PhoneNumber" required>
+                            <label for="Number"><?php echo $text_label_PhoneNumber ?></label>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-6 col-sm-12">
                         <div class="input-box">
-                            <input type="number" spellcheck="false" step="0.01" id="Number" name="tax" required>
-                            <label for="Number">Tax</label>
+                            <input type="password" id="password" name="Password" class="input" spellcheck="false"
+                                required />
+                            <label for="password"><?php echo $text_label_Password ?></label>
+                            <i class="fa-regular fa-eye-slash togglePass"></i>
                         </div>
                     </div>
-
                     <div class="col-md-6 col-lg-6 col-sm-12">
                         <div class="input-box">
-                            <input type="file" name="image">
-                            <label for="image">Image</label>
+                            <input type="password" id="cPassword" name="CPassword" class="input2" spellcheck="false"
+                                required />
+                            <label for="cPassword"><?php echo $text_label_CPassword ?></label>
+                            <i class="fa-regular fa-eye-slash togglePass2"></i>
                         </div>
                     </div>
-
+                    <div class="col-md-6 col-lg-6 col-sm-12">
+                        <div class="input-box">
+                            <input type="email" spellcheck="false" id="email" class="Email" name="Email" maxlength="40"
+                                required>
+                            <label for="email"><?php echo $text_label_Email ?></label>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-sm-12">
+                        <div class="input-box">
+                            <input type="email" spellcheck="false" id="cemail" name="CEmail" maxlength="40" required>
+                            <label for="cemail"><?php echo $text_label_CEmail ?></label>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-sm-12">
+                        <div class="input-box">
+                            <select class="form-select select-box" name="GroupId" aria-label="Default select example">
+                                <option value="" selected><?php echo $text_user_GroupId?></option>
+                                <?php if (false !== $groups): ?>
+                                <?php foreach ($groups as $group):?>
+                                <option value="<?php echo $group->GroupId ?>"><?php echo $group->GroupName ?></option>
+                                <?php endforeach;?>
+                                <?php endif;?>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-sm-12">
+                        <div class="input-box">
+                            <input type="text" spellcheck="false" id="address" name="Address" required>
+                            <label for="address"><?= $text_address_label ?></label>
+                        </div>
+                    </div>
+                    <div class="col-md-12 col-lg-12 col-sm-12">
+                        <div class="input-box">
+                            <input type="file" id="Image" name="Image" />
+                            <label for="Image"><i class="fa-solid fa-cloud-arrow-up"></i><span
+                                    class="ms-4"><?= $text_profile_image ?></span></label>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-sm-12">
+                        <div class="input-box">
+                            <input type="date" spellcheck="false" id="Dob" name="DOB" value="<?= date('Y-m-d') ?>"
+                                required>
+                            <label for="dob"><?= $text_dob_label ?></label>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-sm-12">
+                        <div class="input-box">
+                            <input type="date" spellcheck="false" id="subscriptionDate" name="SubscriptionDate"
+                                value="<?= date('Y-m-d') ?>" required>
+                            <label for="subscriptionDate"><?= $text_joined_label ?></label>
+                        </div>
+                    </div>
                     <div class="col-md-12">
                         <div class="input-box">
-                            <input type="submit" name="submit" value="Save" maxlength="30" required>
+                            <input type="submit" name="submit" value="<?php echo $text_label_save ?>" maxlength="30"
+                                required>
                         </div>
                     </div>
+
+
                 </form>
             </div>
         </div>
--- app/views/users/default.view.php
+++ app/views/users/default.view.php
@@ -1,5 +1,5 @@
 <div class="home-content">
-    <h1 class="main-head"><?= $text_header ;?></h1>
+    <h1 class="main-title text-center"><?= $text_header ;?></h1>
     <div class="main-button">
         <a href="/users/create">
             <span><i class="fa-solid fa-plus me-2 ms-2"></i> <?= $text_new_item ?></span>
@@ -8,18 +8,18 @@
     </div>
     <div class="table-responsive tables">
         <table class="table__content">
-            <thead>
-                <tr>
-                    <th><?= $text_table_username ?></th>
-                    <th><?= $text_table_group ?></th>
-                    <th><?= $text_table_email ?></th>
-                    <th><?= $text_table_subscription_date ?></th>
-                    <th><?= $text_table_last_login ?></th>
-                    <th><?= $text_table_control ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php 
+        <thead>
+            <tr>
+                <th><?= $text_table_username ?></th>
+                <th><?= $text_table_group ?></th>
+                <th><?= $text_table_email ?></th>
+                <th><?= $text_table_subscription_date ?></th>
+                <th><?= $text_table_last_login ?></th>
+                <th><?= $text_table_control ?></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php 
                 if (false !== $users) {
                 foreach ($users as $user) {
                     echo '<tr>' ;
@@ -28,26 +28,23 @@
                         echo '<td>' . $user->Email . '</td>' ;
                         echo '<td>' . $user->SubscriptionDate  . '</td>' ;
                         echo '<td>' . $user->LastLogin . '</td>' ;?>
-                <td>
-                    <a href="/users/edit/<?= $user->UserId ?>" class="btn btn-sm"><i class="fas fa-edit"></i> </a>
-                    <a href="/users/delete/<?= $user->UserId ?>" class="btn btn-sm"
-                        onclick="return confirm('<?= $text_delete_confirm ; ?>');"><i
-                            class="fa-regular fa-trash-can"></i></a>
-                    <a href="/users/resetpassword/<?= $user->UserId ?>" class="btn btn-sm"><i
-                            class="fa-brands fa-keycdn"></i></a>
-                </td>
-                <?php
+                            <td>
+                                <a href="/users/edit/<?= $user->UserId ?>" class="btn btn-sm"><i class="fas fa-edit"></i> </a>
+                                <a href="/users/delete/<?= $user->UserId ?>" class="btn btn-sm"onclick="return confirm('<?= $text_delete_confirm ; ?>');"><i class="fa-regular fa-trash-can"></i></a>
+                                <a href="/users/resetpassword/<?= $user->UserId ?>" class="btn btn-sm"><i class="fa-brands fa-keycdn"></i></a>
+                            </td>
+                    <?php
                     echo '</tr>' ;
                 }
             }
             else {
-                    echo '<tr><td colspan="6" class="alert alert-success text-center mb-2 mt-2">
+                    echo '<tr><td rowspan="6" class="alert alert-success text-center mb-2 mt-2">
                                 <i class="fas fa-exclamation-triangle me-3 "></i> '.$text_no_data.
                         '</td></tr>';
             }
         ?>
-            </tbody>
-        </table>
+        </tbody>
+    </table>
     </div>
 
 </div>
