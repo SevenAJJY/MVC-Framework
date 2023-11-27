20541dc1244b83b00fcd5d154d9e29ac3c0be666
SevenAJJY <yassine.elhajjy@gmail.com>
2023-11-27 17:39:07 +0100

    Added Validate Trait To verify the validity of the input fields

--- app/library/Validate.php
+++ app/library/Validate.php
@@ -38,193 +38,350 @@ trait Validate{
         'url'           => '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/'
     ];
 
-    public function req($value)
-        {
-            return '' != $value || !empty($value);
+    /**
+     * @param mixed $value
+     * 
+     * @return bool
+     */
+    public function req($value): bool
+    {
+        return '' != $value || !empty($value);
+    }
+
+    /**
+     * @param int|float $value
+     * 
+     * @return bool
+     */
+    public function num($value): bool
+    {
+        return (bool) preg_match($this->_regexPatterns['num'] ,$value);
+    }
+
+    /**
+     * @param mixed $value
+     * 
+     * @return int
+     */
+    public function int($value): bool
+    {
+        return (bool) preg_match($this->_regexPatterns['int'] ,$value);
+    }
+
+    /**
+     * @param float $value
+     * 
+     * @return bool
+     */
+    public function float($value): bool
+    {
+        return (bool) preg_match($this->_regexPatterns['float'] ,$value);
+    }   
+
+    /**
+     * @param string $value
+     * 
+     * @return bool
+     */
+    public function alpha($value): bool
+    {
+        return (bool) preg_match($this->_regexPatterns['alpha'] ,$value);
+    }
+
+    /**
+     * @param mixed $value
+     * 
+     * @return bool
+     */
+    public function alphanum($value): bool
+    {
+        return (bool) preg_match($this->_regexPatterns['alphanum'] ,$value);
+    }
+
+    /**
+     * 
+     * function Equal
+     * @param mixed $value
+     * @param mixed $matchAgainst
+     * 
+     * @return bool
+     */
+    public function eq($value , $matchAgainst): bool
+    {
+        return $value == $matchAgainst;
+    }
+
+    /**
+     * function Equal Field
+     * @param mixed $value
+     * @param mixed $otherFieldValue
+     * 
+     * @return bool
+     */
+    public function eq_field($value , $otherFieldValue): bool
+    {
+        return $value == $otherFieldValue;
+    }
+
+    /**
+     *  function Less Than
+     * @param mixed $value
+     * @param mixed $matchAgainst
+     * 
+     * @return bool
+     */
+    public function lt($value , $matchAgainst): bool
+    {
+        if(is_string($value)) {
+            return mb_strlen($value) < $matchAgainst;
+        } elseif (is_numeric($value)) {
+            return $value < $matchAgainst;
         }
+    }
 
-        public function num($value)
-        {
-            return (bool) preg_match($this->_regexPatterns['num'] ,$value);
+    /**
+     * function Greater Than
+     * @param mixed $value
+     * @param mixed $matchAgainst
+     * 
+     * @return bool
+     */
+    public function gt($value , $matchAgainst): bool
+    {
+        if(is_string($value)) {
+            return mb_strlen($value) > $matchAgainst;
+        } 
+        elseif (is_numeric($value)) {
+            return $value > $matchAgainst;
         }
+    }
 
-        public function int($value)
-        {
-            return (bool) preg_match($this->_regexPatterns['int'] ,$value);
+    /**
+     * function Minimum
+     * @param mixed $value
+     * @param mixed $min
+     * 
+     * @return bool | mixed
+     */
+    public function min($value , $min): bool
+    {
+        if(is_string($value)) {
+            return mb_strlen($value) >= $min;
+        } 
+        elseif (is_numeric($value)) {
+            return $value >= $min;
         }
+    }
 
-        public function float($value)
-        {
-            return (bool) preg_match($this->_regexPatterns['float'] ,$value);
-        }   
-
-        public function alpha($value)
-        {
-            return (bool) preg_match($this->_regexPatterns['alpha'] ,$value);
+    /**
+     * function Maximum
+     * @param mixed $value
+     * @param mixed $max
+     * 
+     * @return bool
+     */
+    public function max($value , $max): bool
+    {
+        if (is_String($value)) {
+            return mb_strlen($value) <= $max;
         }
-
-        public function alphanum($value)
-        {
-            return (bool) preg_match($this->_regexPatterns['alphanum'] ,$value);
+        elseif (is_numeric($value)) {
+            return $value <= $max ;
         }
+    }
 
-        /**
-         * 
-         * function Equal
-         * @param mixed $value
-         * @param mixed $matchAgainst
-         * 
-         * @return bool
-         */
-        public function eq($value , $matchAgainst): bool
-        {
-            return $value == $matchAgainst;
+    /**
+     * function between
+     * @param mixed $value
+     * @param mixed $min
+     * @param mixed $max
+     * 
+     * @return bool
+     */
+    public function between($value , $min , $max)
+    {
+        if(is_string($value)) {
+            return mb_strlen($value) >= $min && mb_strlen($value) <= $max;
+        } 
+        elseif (is_numeric($value)) {
+            return $value >= $min && $value <= $max;
         }
+    }
 
-        /**
-         * function Equal Field
-         * @param mixed $value
-         * @param mixed $otherFieldValue
-         * 
-         * @return bool
-         */
-        public function eq_field($value , $otherFieldValue): bool
-        {
-            return $value == $otherFieldValue;
+    /**
+     * Find out how many digits are after the comma and before it in the decimal numbers
+     * @param mixed $value
+     * @param mixed $beforeDP
+     * @param mixed $afterDP
+     * 
+     * @return bool
+     */
+    public function floatlike($value , $beforeDP , $afterDP): bool
+    {
+        if (!$this->float($value)) {
+            return false ;
         }
+        $pattern = '/^[0-9]{'. $beforeDP .'}\.[0-9]{'. $afterDP .'}$/';
+        return (bool) preg_match($pattern ,$value);
+    }
 
-        /**
-         *  function Less Than
-         * @param mixed $value
-         * @param mixed $matchAgainst
-         * 
-         * @return bool
-         */
-        public function lt($value , $matchAgainst): bool
-        {
-            if(is_string($value)) {
-                return mb_strlen($value) < $matchAgainst;
-            } elseif (is_numeric($value)) {
-                return $value < $matchAgainst;
+    /**
+     * validate date (Date format MySQL 'YYYY-MM-DD')
+     * @param mixed $value
+     * 
+     * @return bool
+     */
+    public function vdate($value): bool
+    {
+        return (bool) preg_match($this->_regexPatterns['vdate'], $value);
+    }
+
+    /**
+     * validate Email
+     * @param mixed $value
+     * 
+     * @return bool
+     */
+    public function email($value): bool
+    {
+        return (bool) preg_match($this->_regexPatterns['email'], $value);
+    }
+
+    /**
+     * validate URL
+     * @param mixed $value
+     * 
+     * @return bool
+     */
+    public function url($value): bool
+    {
+        return (bool) preg_match($this->_regexPatterns['url'], $value);
+    }
+
+    public function isValid($roles , $inputType)
+    {
+        $errors = [] ;
+        if (!empty($roles)) {
+            foreach ($roles as $fieldName => $validationRoles) {
+                $value = $inputType[$fieldName] ;
+                $validationRoles = explode('|' , $validationRoles) ;
+                var_dump($validationRoles);
+                // exit;
+                foreach ($validationRoles as $validationRole) {
+                    ### -> To Shows at least one error in the field
+                    ### -> hna f foreach fach tkml roles ila l9at f array dyl errors field name kain using array_key_exists() adir continue bach iduz lvalidation roles li b3dha o ghadi itl3 lina error w7d 3la a9al
+                    if (array_key_exists($fieldName, $errors))  
+                        continue;
+                    /**
+                     * in case of min role
+                     */
+                    if (preg_match_all('/(min)\((\d+)\)/' , $validationRole , $m)) { ## hna ayl9lb  fstring kaml o li l9aha ay7tha f $m 
+                        if ($this->min($value,$m[2][0]) === false) {
+                            $this->messenger->add(
+                                $this->language->feedKey('text_error_'.$m[1][0] , [$this->language->get('text_label_'.$fieldName),$m[2][0]]),
+                                Messenger::APP_MESSAGE_ERROR    
+                            );
+                            $errors[$fieldName] = true;
+                        }
+                    }
+
+                    /**
+                     * in case of max role
+                     */
+                    elseif (preg_match_all('/(max)\((\d+)\)/' , $validationRole , $m)) { 
+                        if ($this->max($value,$m[2][0]) === false) {
+                            $this->messenger->add(
+                                $this->language->feedKey('text_error_' . $m[1][0] , [$this->language->get('text_label_' . $fieldName),$m[2][0]]),
+                                Messenger::APP_MESSAGE_ERROR    
+                            );
+                            $errors[$fieldName] = true;
+                        }
+                    }
+                    /**
+                     * in case of less than role
+                     */
+                    elseif (preg_match_all('/(lt)\((\d+)\)/' , $validationRole , $m)) { 
+                        if ($this->lt($value,$m[2][0]) === false) {
+                            $this->messenger->add(
+                                $this->language->feedKey('text_error_' . $m[1][0] , [$this->language->get('text_label_' . $fieldName),$m[2][0]]),
+                                Messenger::APP_MESSAGE_ERROR    
+                            );
+                            $errors[$fieldName] = true;
+                        }
+                    }
+                    /**
+                     * in case of greater than role
+                     */
+                    elseif (preg_match_all('/(gt)\((\d+)\)/' , $validationRole , $m)) { 
+                        if ($this->gt($value,$m[2][0]) === false) {
+                            $this->messenger->add(
+                                $this->language->feedKey('text_error_' . $m[1][0] , [$this->language->get('text_label_' . $fieldName),$m[2][0]]),
+                                Messenger::APP_MESSAGE_ERROR    
+                            );
+                            $errors[$fieldName] = true;
+                        }
+                    }
+                    /**
+                     * in case of between than role
+                     */
+                    elseif (preg_match_all('/(between)\((\d+),(\d+)\)/' , $validationRole , $m)) { 
+                        if ($this->between($value,$m[2][0],$m[3][0]) === false) {
+                            $this->messenger->add(
+                                $this->language->feedKey('text_error_' . $m[1][0] , [$this->language->get('text_label_' . $fieldName),$m[2][0],$m[3][0]]  ),
+                                Messenger::APP_MESSAGE_ERROR    
+                            );
+                            $errors[$fieldName] = true;
+                        }
+                    }
+                    /**
+                     * in case of floatlike than role
+                     */
+                    elseif (preg_match_all('/(floatlike)\((\d+),(\d+)\)/' , $validationRole , $m)) { 
+                        if ($this->floatlike($value,$m[2][0],$m[3][0]) === false) {
+                            $this->messenger->add(
+                                $this->language->feedKey('text_error_' . $m[1][0] , [$this->language->get('text_label_' . $fieldName),$m[2][0],$m[3][0]]  ),
+                                Messenger::APP_MESSAGE_ERROR    
+                            );
+                            $errors[$fieldName] = true;
+                        }
+                    }
+                    /**
+                     * in case of equal role
+                     */
+                    elseif (preg_match_all('/(eq)\((\w+)\)/' , $validationRole , $m)) { 
+                        if ($this->eq($value,$m[2][0]) === false) {
+                            $this->messenger->add(
+                                $this->language->feedKey('text_error_' . $m[1][0] , [$this->language->get('text_label_' . $fieldName),$m[2][0]]),
+                                Messenger::APP_MESSAGE_ERROR    
+                            );
+                            $errors[$fieldName] = true;
+                        }
+                    }
+                    /**
+                     * in case of equal role
+                     */
+                    elseif (preg_match_all('/(eq_field)\((\w+)\)/' , $validationRole , $m)) {
+                        $otherFieldValue =  $inputType[$m[2][0]] ;
+                        if ($this->eq_field($value,$otherFieldValue) === false) {
+                            $this->messenger->add(
+                                $this->language->feedKey('text_error_' . $m[1][0] , [$this->language->get('text_label_' . $fieldName),$this->language->get('text_label_' . $m[2][0])]),
+                                Messenger::APP_MESSAGE_ERROR    
+                            );
+                            $errors[$fieldName] = true;
+                        }
+                    }
+                    else {
+                        if ($this->$validationRole($value) === false) {
+                            $this->messenger->add(
+                                $this->language->feedKey('text_error_' . $validationRole , [$this->language->get('text_label_' . $fieldName)]),
+                                Messenger::APP_MESSAGE_ERROR    
+                            );
+                            $errors[$fieldName] = true;
+                        }
+                    }
+                }
             }
         }
-
-        /**
-         * function Greater Than
-         * @param mixed $value
-         * @param mixed $matchAgainst
-         * 
-         * @return bool
-         */
-        public function gt($value , $matchAgainst): bool
-        {
-            if(is_string($value)) {
-                return mb_strlen($value) > $matchAgainst;
-            } 
-            elseif (is_numeric($value)) {
-                return $value > $matchAgainst;
-            }
-        }
-
-        /**
-         * function Minimum
-         * @param mixed $value
-         * @param mixed $min
-         * 
-         * @return bool | mixed
-         */
-        public function min($value , $min): bool
-        {
-            if(is_string($value)) {
-                return mb_strlen($value) >= $min;
-            } 
-            elseif (is_numeric($value)) {
-                return $value >= $min;
-            }
-        }
-
-        /**
-         * function Maximum
-         * @param mixed $value
-         * @param mixed $max
-         * 
-         * @return bool
-         */
-        public function max($value , $max): bool
-        {
-            if (is_String($value)) {
-                return mb_strlen($value) <= $max;
-            }
-            elseif (is_numeric($value)) {
-                return $value <= $max ;
-            }
-        }
-
-        /**
-         * function between
-         * @param mixed $value
-         * @param mixed $min
-         * @param mixed $max
-         * 
-         * @return bool
-         */
-        public function between($value , $min , $max)
-        {
-            if(is_string($value)) {
-                return mb_strlen($value) >= $min && mb_strlen($value) <= $max;
-            } 
-            elseif (is_numeric($value)) {
-                return $value >= $min && $value <= $max;
-            }
-        }
-
-        /**
-         * Find out how many digits are after the comma and before it in the decimal numbers
-         * @param mixed $value
-         * @param mixed $beforeDP
-         * @param mixed $afterDP
-         * 
-         * @return bool
-         */
-        public function floatlike($value , $beforeDP , $afterDP): bool
-        {
-            if (!$this->float($value)) {
-                return false ;
-            }
-            $pattern = '/^[0-9]{'. $beforeDP .'}\.[0-9]{'. $afterDP .'}$/';
-            return (bool) preg_match($pattern ,$value);
-        }
-
-        /**
-         * validate date (Date format MySQL 'YYYY-MM-DD')
-         * @param mixed $value
-         * 
-         * @return bool
-         */
-        public function vdate($value): bool
-        {
-            return (bool) preg_match($this->_regexPatterns['vdate'], $value);
-        }
-
-        /**
-         * validate Email
-         * @param mixed $value
-         * 
-         * @return bool
-         */
-        public function email($value): bool
-        {
-            return (bool) preg_match($this->_regexPatterns['email'], $value);
-        }
-
-        /**
-         * validate URL
-         * @param mixed $value
-         * 
-         * @return bool
-         */
-        public function url($value): bool
-        {
-            return (bool) preg_match($this->_regexPatterns['url'], $value);
-        }
+        return empty($errors) ? true : false ;
+    }
 }
--- app/library/language.php
+++ app/library/language.php
@@ -34,4 +34,18 @@ class Language{
     public function getDictionary(){
         return $this->_dictionary;
     }
+
+    public function get($key){
+        if(array_key_exists($key, $this->_dictionary)){
+            return $this->_dictionary[$key];
+        }
+    }
+
+    public function feedKey($key, $data)
+    {
+        if (array_key_exists($key ,$this->getDictionary())) {
+            array_unshift($data , $this->_dictionary[$key]);
+            return call_user_func_array('sprintf',$data);
+        }
+    } 
 }
