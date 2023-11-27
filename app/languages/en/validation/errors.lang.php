d6f1bc6755b90a814a64632b030141f1c50ff86e
SevenAJJY <yassine.elhajjy@gmail.com>
2023-11-27 17:37:15 +0100

    Added language files That appears when you enter something unwanted

---
+++ app/languages/en/validation/errors.lang.php
@@ -0,0 +1,19 @@
+<?php 
+
+    $_['text_error_req']       = 'Field %s must contain a value';
+    $_['text_error_num']       = 'Field %s must contain a number';
+    $_['text_error_int']       = 'Field %s must contain an integer number';
+    $_['text_error_float']     = 'Field %s must contain a decimal number';
+    $_['text_error_alpha']     = 'Field %s must contain only characters';
+    $_['text_error_alphanum']  = 'Field %s must contain letters and numbers only' ;
+    $_['text_error_eq']        = '%s must be equal to %s' ;
+    $_['text_error_eq_field']  = '%s must be equal to %s'  ;
+    $_['text_error_lt']        = 'Field %s must be less than %s' ;
+    $_['text_error_gt']        = 'Field %s must be more than %s';
+    $_['text_error_min']       = 'Field %s must have a value of at least %s (characters if text)';
+    $_['text_error_max']       = 'Field %s must have a maximum value of %s (characters if text)';
+    $_['text_error_between']   = 'Field %s must have a value between %s and %s (characters if text)';
+    $_['text_error_floatlike'] = 'Field %s must have a value between %s and %s (characters if text)';
+    $_['text_error_vdate']     = 'Field %s must be a date in YYYY-mm-dd format';
+    $_['text_error_email']     = 'Field %s must be an email address Ex: someone@mail.com';
+    $_['text_error_url']       = 'Field %s must be a valid link Ex: http://www.abc.com';
