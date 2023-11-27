dbfcee18ad36a1aa835b9a6591af37a1847e23bc
SevenAJJY <yassine.elhajjy@gmail.com>
2023-11-27 17:38:01 +0100

    Added user profile model

---
+++ app/models/userprofilemodel.php
@@ -0,0 +1,30 @@
+<?php
+namespace SEVENAJJY\Models ;
+class UserProfileModel extends AbstractModel
+    {
+        public $UserId ;
+
+        public $FirstName ;
+
+        public $LastName ; 
+
+        public $Address ; 
+
+        public $DOB ; 
+    
+        public $Image ;
+
+        protected static $tableName = 'app_users_profiles' ;
+
+        protected static $tableSchema = array(
+            'UserId'            => self::DATA_TYPE_INT, 
+            'FirstName'         => self::DATA_TYPE_STR, 
+            'LastName'          => self::DATA_TYPE_STR,
+            'Address'           => self::DATA_TYPE_STR,
+            'DOB'               => self::DATA_TYPE_STR,
+            'Image'             => self::DATA_TYPE_STR
+        );
+
+        protected static $primaryKey = 'UserId' ;
+
+    }
