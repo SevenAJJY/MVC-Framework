ea6e50d48cd5f1283834007841abd1929e82419f
SevenAJJY <yassine.elhajjy@gmail.com>
2023-11-29 12:11:08 +0100

    The showValue method was added to help us when we make errors in Fields Version 1 & 2

--- app/library/template/templateHelper.php
+++ app/library/template/templateHelper.php
@@ -5,4 +5,30 @@ trait TemplateHelper {
     public function matchURL($url){
         return parse_url($_SERVER['REQUEST_URI'] , PHP_URL_PATH) === $url;
     }
+
+    /**
+     * function to help me when I made a mistake in a field version 1
+     * 
+     * @param mixed $fielfName
+     * @param object $object
+     * 
+     * @return mixed
+     */
+    public function showValueV1($fielfName , $object = null)
+    {
+        return isset($_POST[$fielfName]) ? $_POST[$fielfName] : (is_null($object) ? '' : $object->$fielfName) ; 
+    }
+
+    /**
+     * function to help me when I made a mistake in a field version 2
+     *
+     * @param string $fieldName
+     * @param object $object
+     * @param boolean $defaultValue
+     * @return mixed
+     */
+    public function showValueV2($fieldName, $object = null, $defaultValue = false): mixed
+    {
+        return isset($_POST[$fieldName]) ? $_POST[$fieldName] : ($object === null ? ($defaultValue === false ? '' : $defaultValue) : $object->$fieldName);
+    }
 }
--- app/views/users/create.view.php
+++ app/views/users/create.view.php
@@ -1,74 +1,77 @@
 <div class="home-content">
     <div class="row">
-        <h1 class="main-head"><?php echo $text_header ;?></h1>
+        <h1 class="main-head"><?= $text_header ;?></h1>
 
         <div class="col-md-12 d-flex align-items-center justify-content-center">
             <div class="my-container fuser">
-                <h4><?php echo $text_legend ?></h4>
+                <h4><?= $text_legend ?></h4>
                 <form action="" class="appform row" method="post" autocomplete="off" enctype="multipart/form-data"
                     enctype="application/x-www-form-urlencoded">
                     <div class="col-md-6 col-lg-6 col-sm-12">
                         <div class="input-box">
                             <input type="text" spellcheck="false" class="FirstName" name="FirstName" id="FirstName"
-                                maxlength="10" required>
-                            <label for="FirstName"><?php echo $text_label_FirstName ?></label>
+                                maxlength="10" value="<?= $this->showValueV2('FirstName') ?>" required>
+                            <label for="FirstName"><?= $text_label_FirstName ?></label>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-6 col-sm-12">
                         <div class="input-box">
-                            <input type="text" spellcheck="false" name="LastName" id="LastName" maxlength="10" required>
-                            <label for="LastName"><?php echo $text_label_LastName ?></label>
+                            <input type="text" spellcheck="false" name="LastName" id="LastName" maxlength="10"
+                                value="<?= $this->showValueV2('LastName') ?>" required>
+                            <label for="LastName"><?= $text_label_LastName ?></label>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-6 col-sm-12">
                         <div class="input-box">
                             <input type="text" spellcheck="false" class="Username" name="Username" id="Username"
-                                maxlength="30" required>
-                            <label for="username" class="labelerror"><?php echo $text_label_Username ?></label>
+                                maxlength="30" value="<?= $this->showValueV2('Username') ?>" required>
+                            <label for="username" class="labelerror"><?= $text_label_Username ?></label>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-6 col-sm-12">
                         <div class="input-box">
-                            <input type="text" spellcheck="false" id="Number" name="PhoneNumber" required>
-                            <label for="Number"><?php echo $text_label_PhoneNumber ?></label>
+                            <input type="text" spellcheck="false" id="Number" name="PhoneNumber"
+                                value="<?= $this->showValueV2('PhoneNumber') ?>" required>
+                            <label for="Number"><?= $text_label_PhoneNumber ?></label>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-6 col-sm-12">
                         <div class="input-box">
                             <input type="password" id="password" name="Password" class="input" spellcheck="false"
-                                required />
-                            <label for="password"><?php echo $text_label_Password ?></label>
+                                value="<?= $this->showValueV2('Password') ?>" required />
+                            <label for="password"><?= $text_label_Password ?></label>
                             <i class="fa-regular fa-eye-slash togglePass"></i>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-6 col-sm-12">
                         <div class="input-box">
                             <input type="password" id="cPassword" name="CPassword" class="input2" spellcheck="false"
-                                required />
-                            <label for="cPassword"><?php echo $text_label_CPassword ?></label>
+                                value="<?= $this->showValueV2('CPassword') ?>" required />
+                            <label for="cPassword"><?= $text_label_CPassword ?></label>
                             <i class="fa-regular fa-eye-slash togglePass2"></i>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-6 col-sm-12">
                         <div class="input-box">
                             <input type="email" spellcheck="false" id="email" class="Email" name="Email" maxlength="40"
-                                required>
-                            <label for="email"><?php echo $text_label_Email ?></label>
+                                value="<?= $this->showValueV2('Email') ?>" required>
+                            <label for="email"><?= $text_label_Email ?></label>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-6 col-sm-12">
                         <div class="input-box">
-                            <input type="email" spellcheck="false" id="cemail" name="CEmail" maxlength="40" required>
-                            <label for="cemail"><?php echo $text_label_CEmail ?></label>
+                            <input type="email" spellcheck="false" id="cemail" name="CEmail" maxlength="40"
+                                value="<?= $this->showValueV2('CEmail') ?>" required>
+                            <label for="cemail"><?= $text_label_CEmail ?></label>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-6 col-sm-12">
                         <div class="input-box">
                             <select class="form-select select-box" name="GroupId" aria-label="Default select example">
-                                <option value="" selected><?php echo $text_user_GroupId?></option>
+                                <option value="" selected><?= $text_user_GroupId?></option>
                                 <?php if (false !== $groups): ?>
                                 <?php foreach ($groups as $group):?>
-                                <option value="<?php echo $group->GroupId ?>"><?php echo $group->GroupName ?></option>
+                                <option value="<?= $group->GroupId ?>"><?= $group->GroupName ?></option>
                                 <?php endforeach;?>
                                 <?php endif;?>
                             </select>
@@ -76,13 +79,14 @@
                     </div>
                     <div class="col-md-6 col-lg-6 col-sm-12">
                         <div class="input-box">
-                            <input type="text" spellcheck="false" id="address" name="Address" required>
+                            <input type="text" spellcheck="false" id="address" name="Address"
+                                value="<?= $this->showValueV2('Address') ?>" required>
                             <label for="address"><?= $text_address_label ?></label>
                         </div>
                     </div>
                     <div class="col-md-12 col-lg-12 col-sm-12">
                         <div class="input-box">
-                            <input type="file" id="Image" name="Image" />
+                            <input type="file" id="Image" name="Image" value="<?= $this->showValueV2('Image') ?>" />
                             <label for="Image"><i class="fa-solid fa-cloud-arrow-up"></i><span
                                     class="ms-4"><?= $text_profile_image ?></span></label>
                         </div>
@@ -103,8 +107,7 @@
                     </div>
                     <div class="col-md-12">
                         <div class="input-box">
-                            <input type="submit" name="submit" value="<?php echo $text_label_save ?>" maxlength="30"
-                                required>
+                            <input type="submit" name="submit" value="<?= $text_label_save ?>" maxlength="30" required>
                         </div>
                     </div>
 
