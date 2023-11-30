920ac68a1307980b4f6c93cb81aa5fbe4370d78e
SevenAJJY <yassine.elhajjy@gmail.com>
2023-11-30 20:07:27 +0100

    Added language files of Supplier Controller

---
+++ app/languages/en/suppliers/labels.lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$_['text_legend'] = 'Supplier Details';
+$_['text_label_Name'] = 'Supplier Name';
+$_['text_label_PhoneNumber'] = 'Phone Number';
+$_['text_label_Email'] ='Email';
+$_['text_label_Address'] = 'Address';
+$_['text_label_PartitaIVA'] = "VAT number";
+$_['text_label_CodFISC'] = "Tax ID code";
+
+$_['text_label_save'] = 'Save';
