36b85c71431a25d722e41b8159dbdc0f730657ab
SevenAJJY <yassine.elhajjy@gmail.com>
2023-12-04 18:42:39 +0100

    Added language files of the Clients controller

---
+++ app/languages/en/clients/labels.lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$_['text_legend'] = 'Client Details';
+$_['text_label_Name'] = 'Client Name';
+$_['text_label_PhoneNumber'] = 'Phone Number';
+$_['text_label_Email'] ='Email';
+$_['text_label_Address'] = 'Address';
+$_['text_label_PartitaIVA'] = "VAT number";
+$_['text_label_CodFISC'] = "Tax ID code";
+$_['text_label_save'] = 'Save';
