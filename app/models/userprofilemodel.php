32d9f4bd541f2f6ab4c55d7e79e1ef5f740d59ec
SevenAJJY <yassine.elhajjy@gmail.com>
2023-12-11 11:48:32 +0100

    Added Method to all information of the user profile

--- app/models/userprofilemodel.php
+++ app/models/userprofilemodel.php
@@ -1,30 +1,34 @@
 <?php
 namespace SEVENAJJY\Models ;
 class UserProfileModel extends AbstractModel
+{
+    public $UserId ;
+
+    public $FirstName ;
+
+    public $LastName ; 
+
+    public $Address ; 
+
+    public $DOB ; 
+
+    public $Image ;
+
+    protected static $tableName = 'app_users_profiles' ;
+
+    protected static $tableSchema = array(
+        'UserId'            => self::DATA_TYPE_INT, 
+        'FirstName'         => self::DATA_TYPE_STR, 
+        'LastName'          => self::DATA_TYPE_STR,
+        'Address'           => self::DATA_TYPE_STR,
+        'DOB'               => self::DATA_TYPE_STR,
+        'Image'             => self::DATA_TYPE_STR
+    );
+
+    protected static $primaryKey = 'UserId' ;
+
+    public static function getProfile($user)
     {
-        public $UserId ;
-
-        public $FirstName ;
-
-        public $LastName ; 
-
-        public $Address ; 
-
-        public $DOB ; 
-    
-        public $Image ;
-
-        protected static $tableName = 'app_users_profiles' ;
-
-        protected static $tableSchema = array(
-            'UserId'            => self::DATA_TYPE_INT, 
-            'FirstName'         => self::DATA_TYPE_STR, 
-            'LastName'          => self::DATA_TYPE_STR,
-            'Address'           => self::DATA_TYPE_STR,
-            'DOB'               => self::DATA_TYPE_STR,
-            'Image'             => self::DATA_TYPE_STR
-        );
-
-        protected static $primaryKey = 'UserId' ;
-
+        return self::getOne('SELECT * FROM '. self::$tableName . ' WHERE UserId = ' . $user->UserId);
     }
+}
