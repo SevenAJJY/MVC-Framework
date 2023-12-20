b7a753e81e6e859c6ff11bfc032d3ff19a252477
SevenAJJY <yassine.elhajjy@gmail.com>
2023-12-20 12:29:07 +0100

    Added files language of sales controller

--- app/languages/ar/sales/view.lang.php
+++ app/languages/ar/sales/view.lang.php
@@ -1,15 +1,29 @@
 <?php
 $_['title'] = 'فاتورة مبيعات ';
 $_['text_header'] = 'فاتورة مبيعات ';
-$_['text_name_label'] = 'إسم العميل';
+$_['text_name'] = 'إسم العميل';
 $_['text_discount_percentage'] = 'نسبة الخصم % ';
 $_['text_product_name'] = 'اسم الصنف';
-$_['text_product_quantity'] = 'الكمية';
+$_['text_product_quantity'] = 'الكمية الموردة';
 $_['text_product_price'] = 'السعر للوحدة';
-$_['text_payment_type_label'] = 'طريقة الدفع';
-$_['text_payment_status_label'] = 'حالة الدفع';
+$_['text_payment_type'] = 'طريقة الدفع';
+$_['text_payment_status'] = 'حالة الدفع';
 $_['text_table_ref'] = 'رقم الفاتورة';
 $_['text_created'] = 'التاريخ';
+$_['text_product_list'] = 'لائحة المنتجات';
+
+$_['text_unit'] = 'الوحدة';
+$_['text_Quantity'] = 'الكمية';
+$_['text_pieces'] = 'القطع';
+$_['text_price'] = 'السعر';
+$_['text_piecesBox'] = 'قطعة/صندوق';
+$_['text_total'] = 'المجموع';
+$_['text_total_end'] = ' DOC € المجموع';
+
+
 
 $_['text_payment_type_1'] = 'نقدا';
 $_['text_payment_type_2'] = 'آجل';
+
+$_['text_payment_status_1'] = 'مدفوعة كليا';
+$_['text_payment_status_2'] = 'مدفوعة جزئيا';
--- app/languages/en/sales/view.lang.php
+++ app/languages/en/sales/view.lang.php
@@ -2,15 +2,31 @@
 
 $_['title'] = 'sales bill';
 $_['text_header'] = 'sales bill';
-$_['text_name_label'] = 'Client name';
+$_['text_name'] = 'Client name';
 $_['text_discount_percentage'] = 'discount percentage %';
 $_['text_product_name'] = 'Product Name';
-$_['text_product_quantity'] = 'Quantity';
+$_['text_product_quantity'] = 'Supplied Quantity';
 $_['text_product_price'] = 'price per unit';
-$_['text_payment_type_label'] = 'Payment method';
-$_['text_payment_status_label'] = 'Payment status';
+$_['text_payment_type'] = 'Payment method';
+$_['text_payment_status'] = 'Payment status';
 $_['text_table_ref'] = 'invoice number';
 $_['text_created'] = 'Date';
+$_['text_product_list'] = 'Product list';
+
+$_['text_unit'] = 'Unit';
+$_['text_Quantity'] = 'Quantity';
+$_['text_pieces'] = 'Pieces';
+$_['text_price'] = 'Price';
+$_['text_total'] = 'Total';
+$_['text_total_end'] = 'TOT. DOC €';
+
+
+$_['text_piecesBox'] = 'Pieces/Box';
+
+
+
 
 $_['text_payment_type_1'] = 'cash Money';
-$_['text_payment_type_2'] = 'credit';
+$_['text_payment_type_2'] = 'pay on credit';
+$_['text_payment_status_1'] = 'all driven';
+$_['text_payment_status_2'] = 'Partially driven';
