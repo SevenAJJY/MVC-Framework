e3961ba9188ea4dc3aa13a8248019d80cc1ce5e0
SevenAJJY <yassine.elhajjy@gmail.com>
2024-01-14 16:52:44 +0100

    Added some word to the file language

--- app/languages/ar/users/settings.lang.php
+++ app/languages/ar/users/settings.lang.php
@@ -6,3 +6,5 @@ $_['text_header'] = 'اعدادات اخرى';
 $_['text_change_photo'] = 'تغيير الصورة';
 $_['text_recent_activity'] = 'النشاط الأخير';
 $_['text_other_settings'] = 'اعدادات اخرى';
+$_['text_hide'] = 'إخفاء';
+$_['text_style_switcher'] = 'مبدل الألوان';
--- app/languages/en/users/settings.lang.php
+++ app/languages/en/users/settings.lang.php
@@ -2,18 +2,13 @@
 
 $_['title'] = 'Settings';
 $_['text_header'] = 'Settings';
-$_['text_firstname'] = 'Fisrt name';
-$_['text_lastname'] = 'Last name';
-$_['text_email'] = 'Email';
-$_['text_dob'] = 'Birthday';
-$_['text_address'] = 'Address';
-$_['text_phone'] =  'Phone Number';
-$_['text_profile'] = 'Profile';
-$_['text_edit_profile'] = 'Edit profile';
-$_['text_change_password'] = 'Change Password';
+
+
 $_['text_recent_activity'] = 'Recent Activity';
 $_['text_subscription_date'] = 'Subscription Date';
 $_['text_total_privileges'] = 'Total Privileges';
 $_['text_change_photo'] = 'Change photo';
 $_['text_other_settings'] = 'other settings';
+$_['text_hide'] = 'Hide';
+$_['text_style_switcher'] = 'Style Switcher';
 $_['text_group'] = 'Group';
