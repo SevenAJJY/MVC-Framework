59cabdaef1228d315f33d5c8c473e26c819377b9
SevenAJJY <yassine.elhajjy@gmail.com>
2024-01-14 22:46:09 +0100

    Added dompdf class to help us for generate pdf files

--- app/views/index/default.view.php
+++ app/views/index/default.view.php
@@ -1,3 +1,11 @@
 <div class="home-content"">
     <h1 class=" main-head"><?= $text_header ;?></h1>
+    <div class="row">
+        <div class="col-md-6 info_logo">
+            <div style="font-size: 30px; font-weight: 600;color:var(--text-color)">Hi, Sevenajjy</div>
+        </div>
+        <div class="col-md-6 ">
+            <div></div>
+        </div>
+    </div>
 </div>
